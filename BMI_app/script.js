ae909b4569ffc8e1067cbe6f5ebf64ae27b32cfe
ryota37 <ryota.nakahama@access-company.com>
2024-08-03 16:22:17 +0900

    minimum implementation of BMI apps

--- BMI_app/script.js
+++ BMI_app/script.js
@@ -1,1 +1,7 @@
-// Add your code here
+function calcBMI(){
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+    const rawBMI = weight / (height * height);
+    const BMI = Math.round(rawBMI * 100) / 100;
+    document.getElementById("result").innerText = "BMI: " + BMI;
+}
