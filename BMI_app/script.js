5624e42373b942d532011005eecfedaf8fca154e
ryota37 <ryota.nakahama@access-company.com>
2024-08-10 11:32:04 +0900

    Implement BMI classification

--- BMI_app/script.js
+++ BMI_app/script.js
@@ -4,4 +4,18 @@ function calcBMI(){
     const rawBMI = weight / (height * height);
     const BMI = Math.round(rawBMI * 100) / 100;
     document.getElementById("result").innerText = "BMI: " + BMI;
+
+    let classification;
+
+    if(BMI < 18.5){
+        classification = "Underweight";
+    } else if(BMI >= 18.5 && BMI < 24.9){
+        classification = "Normal weight";
+    } else if(BMI >= 25 && BMI < 29.9){
+        classification = "Overweight";
+    } else if(BMI >= 30){
+        classification = "Obesity";
+    }
+
+    document.getElementById("classification").innerText = "Classification: " + classification;
 }
